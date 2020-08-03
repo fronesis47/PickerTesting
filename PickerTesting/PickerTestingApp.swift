f2ec53c524ce53ca29f1916c061dfa5c0f648cbd
Samuel Chambers <fronesis47@mac.com>
2020-08-03 10:50:45 -0400

    Initial Commit

---
+++ PickerTesting/PickerTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerTestingApp.swift
+//  PickerTesting
+//
+//  Created by Samuel Chambers on 8/3/20.
+//
+
+import SwiftUI
+
+@main
+struct PickerTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
