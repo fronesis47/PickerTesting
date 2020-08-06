aa8a82393ffc5c9e6956e777cbfbf81e996d258b
Samuel Chambers <fronesis47@mac.com>
2020-08-05 21:45:09 -0400

    Before Paul made me break it.

--- PickerTesting/View/ContentView.swift
+++ PickerTesting/View/ContentView.swift
@@ -3,66 +3,48 @@ import SwiftUI
 
 struct ContentView: View {
 
-    let brands = getBrandsAll()
-
+    @State private var brands = getBrandsAll()
 
     @State private var brand = Brand()
     @State private var model = Model()
-    
     @State private var chosenBrand = 0
     @State private var chosenModel = 0
     @State private var chosenYear = 0
     
-    
     var body: some View {
-        
-        
-        
- // I think these pickers are CORRECT; they just need cleaned data
         VStack {
-            
             Picker(selection: $chosenBrand, label: Text("Brand")) {
                 ForEach(0..<self.brands.count) { index in
                     Text(self.brands[index].name).tag(index)
                     }
                 }
-            .onChange(of: chosenBrand) { _ in brand = self.brands[chosenBrand]; chosenModel = 0 }
+                .labelsHidden()
+                .onChange(of: chosenBrand) { _ in brand = self.brands[chosenBrand];
+                    chosenModel = 0 }
             
             Picker(selection: $chosenModel, label: Text("Model")) {
                 ForEach(0..<brand.models.count) { index in
                     Text(brand.models[index].name).tag(index)
                     }
-            }
-            .onChange(of: chosenModel) { _ in model = self.brands[chosenBrand].models[chosenModel] }
-            .id(brand)
+                }
+            .onChange(of: chosenModel) { _ in model = self.brands[chosenBrand].models[chosenModel] ; chosenYear = 0 }
+                .id(brand)
+                .labelsHidden()
             
             Picker(selection: $chosenYear, label: Text("Year")) {
                 ForEach(0..<model.years.count) { index in
-                    Text(model.years[index]).tag(index)
+                    Text(model.years[index].description).tag(index)
                     }
-            }
-            .id(brand)
+                }
+            .labelsHidden()
+            .id(model)
         }
     }
 }
 
 
-
-        
-//        Text(brands[0].name)
-//
-//        Text(brands[1].models[0].name)
-//
-//        Text(brands[2].models[0].years[0])
-        
-        
-
-        
-
-
-
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- PickerTesting/ViewModel/ViewModelParamaters.swift
+++ PickerTesting/ViewModel/ViewModelParamaters.swift
@@ -4,16 +4,9 @@
 //
 //  Created by Samuel Chambers on 8/3/20.
 //  Copyright © 2020 Samuel Chambers. All rights reserved.
-//
 
 import Foundation
 import Combine
-
-// the data is right; i just need to figure out how to use it
-
-
-
-
 
 struct Brand: Hashable, Comparable {
     static func == (lhs: Brand, rhs: Brand) -> Bool {
@@ -28,7 +21,7 @@ struct Brand: Hashable, Comparable {
     var models: [Model]
 
     init () {
-        self.name = "yourMom"
+        self.name = "Bob"
         self.models = []
     }
     
@@ -36,12 +29,6 @@ struct Brand: Hashable, Comparable {
         self.name = name
         self.models = models
     }
-
-// do I need same for years in models, or will they come along for the ride?
-    mutating func add(model: Model) {
-      models.append(model)
-    }
-    
 }
 
 struct Model: Hashable, Comparable {
@@ -54,218 +41,92 @@ struct Model: Hashable, Comparable {
     }
 
     var name: String
-    var years: [String]
+    var years: [Int]
+
+//this should allow me to add to years: var brand = Brand(); brand.models.addYear(2022)
+    mutating func addYear(year: Int) {
+      years += [year]
+    }
     
+
+// void init could be []; generic parameters init required self
     init () {
-        
         self.name = "Paul"
         self.years = []
     }
 
-    init(name:String, years:[String]) {
+    init(name: String, years: [Int]) {
         self.name = name
         self.years = years
     }
 }
 
-//ORIGINAL – gets data but it's a mess
-//func getBrandsAll() -> [Brand] {
-//    var brands: [Brand] = []
-//    let allFrames = frameData
-//        for i in 0..<allFrames.count {
-//            brands.append(Brand(name: allFrames[i].brand, models: [Model(name:  allFrames[i].model, years: [allFrames[i].year.description])]))
-//        }
-//        let unique = Array(Set(brands))
-//        let sorted = unique.sorted()
-////        dump(sorted)
-//        return sorted
-//}
 
 
-// Paul flavor
 
 func getBrandsAll() -> [Brand] {
     var brands: [String : Brand] = [:]
     var modelHelper: [String : [String : Int]] = [:]
     let allFrames = frameData
-
+    
     for i in 0..<allFrames.count {
         if brands[allFrames[i].brand] == nil {
-            brands[allFrames[i].brand] = Brand(name: allFrames[i].brand, models: [Model(name: allFrames[i].model, years: [allFrames[i].year.description])])
+            brands[allFrames[i].brand] = Brand(name: allFrames[i].brand, models: [Model(name: allFrames[i].model, years: [allFrames[i].year])])
 
-//note the index of the model within the brand
-    modelHelper[allFrames[i].brand] = [allFrames[i].model: 0]
+                modelHelper[allFrames[i].brand] = [allFrames[i].model : 0]
+
         } else if modelHelper[allFrames[i].brand]?[allFrames[i].model] == nil {
-            //we have the brand, but not the model, so add that
-          brands[allFrames[i].brand]?.models.append(Model(name: allFrames[i].model, years: [allFrames[i].year.description]))
+//we have the brand, but not the model, so add that
+//SC: OK, but if clause adds brand/model AND year; here we add model AND year to same brand
+              brands[allFrames[i].brand]?.models.append(Model(name: allFrames[i].model, years: [allFrames[i].year]))
+
+//Must be set non-nil; avoids repeating models. Old code complex; for years block too; not needed
+             modelHelper[allFrames[i].brand]?[allFrames[i].model] = (brands[allFrames[i].brand]?.models.count)! - 1
+//            modelHelper[allFrames[i].brand]?[allFrames[i].model] = 0
+//  dump(modelHelper)
+
+//MARK: years did not work
+// This code block below was firing every time we iterated and found a brand and model present
+// it then appended a year..but there was already a year there (added under the if or the else if above)
+// so for each model in allFrames, we did NOT repeat the brand or model, but we DID ADD another year to the years array
+
+// BOTTOM LINE: I see no way that with ONE iteration through allFrames it's possible to do both of these things:
+// 1) NOT repeat models; 2) get multiple years for same model name.
+// Easy soluton is to just deal with this in the data (new model year gets unique model name)
+// But I'm curious how it could be dealt with in the code. I can only think of doing it AFTER the fact, i.e.
+// by going back through all frames again to find same model/new year combos, then APPEND them
+// though this might be EASIER to do while the data is still in dictionary form – not sure
+
             
-            //and note the index where this model appears
-            modelHelper[allFrames[i].brand]?[allFrames[i].model] = (brands[allFrames[i].brand]?.models.count)! - 1
 //        } else {
-////we have this brand and this model, so we just need to add the year
+////
 //            let modelIdx = modelHelper[allFrames[i].brand]?[allFrames[i].model] ?? 0
 //            print(modelIdx)
 //            brands[allFrames[i].brand]?.models[modelIdx].years.append(allFrames[i].year.description)
+//////////
+            
             }
     }
     
-        
-    
-//the only thing that might not be unique at this point is the years
-//that's non-trivial, but ima let you figure that out
-
+//    dump(brands)
         var brandslist: [Brand] = []
         
         for (_,value) in brands {
-            brandslist.append(value)
+                brandslist.append(value)
+            
         }
-    
         let sorted = brandslist.sorted()
         dump(sorted)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
         return sorted
 }
-
-
-
-
-// the idea here was to collect all the models belonging to one brand in the newModels array
-// then when we came to a new brand, write the models into the current brand
-// This doesn't deal with brands only listed once (Orbea)
-// and right now the loop just keeps going repeating cannondale and canyon over and over
-
-//func combineSort (uglyData: [Brand]) -> [Brand] {
-//    var cleanData: [Brand] = []
-//    var newModels = [Model]()
-//    var counter = 0
-//        while counter < 11 {
-//            if uglyData[counter].name == uglyData[counter+1].name {
-//                newModels.append(contentsOf: uglyData[counter].models)
-////                dump(newModels)
-//                }
-//
-//
-//            else if uglyData[counter].name != uglyData[counter+1].name {
-//                cleanData.append(Brand(name: uglyData[counter].name, models: newModels))
-////                cleanData.append(Brand(name: uglyData[counter+1].name, models: [Model(name: uglyData[counter+1].models[0].name, years: uglyData[counter+1].models[0].years)]))
-//
-//
-//                    newModels = []
-//                }
-//                counter += 1
-//        }
-////    let unique = Array(Set(cleanData)) //this attempt to remove duplicates failed (why????)
-//    dump(cleanData)
-//    return cleanData
-//}
-
-
-// this begins to combine models into brands, but it creates EVERY possible
-// combinaton of two models, and it eliminates nothing
-//func combineSort (uglyData: [Brand]) -> [Brand] {
-//    var cleanData: [Brand] = []
-////    var counter = 0
-//    for i in 0..<uglyData.count {
-//        for j in 1..<(uglyData.count - 1) {
-//            if uglyData[i].name == uglyData[j].name {
-//                cleanData.append(Brand(name: uglyData[i].name, models: [Model(name: uglyData[i].models[0].name, years: [uglyData[i].models[0].years[0].description]),Model(name: uglyData[j].models[0].name, years: [uglyData[j].models[0].years[0].description])]))
-////                counter += 1
-//                }
-////            counter = 0
-//        }
-//    }
-//    dump(cleanData)
-//    return cleanData
-//}
-
-
-
-
-
-//
-//
-//
-//class SearchArrays: ObservableObject {
-//
-//    var brand: String
-//    var model: String
-//    var year: Int
-//
-//    init() {
-//        self.brand = ""
-//        self.model = ""
-//        self.year = 0
-//    }
-//
-//    @Published var brands = getBrands()
-//    @Published var models = getAllModels()
-//    @Published var years = getAllYears()
-//}
-//
-//
-//
-//func getBrands() -> [String] {
-//    var brands: [String] = []
-//    let allFrames = frameData
-//        for i in 0..<allFrames.count {
-//            brands.append(allFrames[i].brand)
-//        }
-//    let setBrands = Set(brands)
-//    brands = Array(setBrands)
-//    return brands.sorted()
-//
-//}
-//
-//func getAllModels() -> [String] {
-//    var models: [String] = []
-//    let allFrames = frameData
-//        for i in 0..<allFrames.count {
-//            models.append(allFrames[i].model)
-//        }
-//        let setModels = Set(models)
-//        models = Array(setModels)
-//        return models.sorted()
-//}
-//
-//
-//func getAllYears() -> [Int] {
-//    var years: [Int] = []
-//    let allFrames = frameData
-//    for i in 0..<allFrames.count {
-//        years.append(allFrames[i].year)
-//    }
-//    let setYears = Set(years)
-//    years = Array(setYears)
-//    return years.sorted().reversed()
-//}
-//
-//
-//func getModelsFromBrand (brand: String) -> [String] {
-//    var models: [String] = []
-//    let allFrames = frameData
-//    for i in 0..<allFrames.count {
-//        if allFrames[i].brand == brand {
-//            models.append(allFrames[i].model)
-//        }
-//    }
-//        let setModels = Set(models)
-//        models = Array(setModels)
-//        return models.sorted()
-//}
-//
-//func getYearsFromModels (model: String) -> [Int] {
-//    var years: [Int] = []
-//    let allFrames = frameData
-//    for i in 0..<allFrames.count {
-//        if allFrames[i].model == model {
-//            years.append(allFrames[i].year)
-//        }
-//    }
-//    let setYears = Set(years)
-//    years = Array(setYears)
-//    return years.sorted().reversed()
-//}
-//
-
-
-
 
